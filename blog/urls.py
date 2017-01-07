66592b3cd15fa78b2ae1d2d3326b8c9b2acaafe2
aniket03 <aniket.bhatnagar0310@gmail.com>
2017-01-07 20:18:35 +0530

    Added post_detail view template and url with css for site

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list ,name = 'post_list'),
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail ,name = 'post_detail'),
 	## the last part name=post_list that will be used to dentify the view.
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.utils import timezone
 
+## This function takes a request and will return render (put together) a template
+## blog/post_list.html
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request,'blog/post_list.html',{'posts':posts} )
-## This function tales a request and will return render (put tpgether) a template
-## blog/post_list.html
+	##Here a list is passed to template file
+
+##pk is the variable we send from urls and should not be ommitted and should be kept same
+def post_detail(request,pk):
+	##post = Post.objects.get(pk=pk)	
+	##This could have been used but if pk does not exist then it should give 404 error so we use
+	##so we use django shortcut
+	post = get_object_or_404(Post,pk=pk)
+	return render(request,'blog/post_detail.html',{'post':post} )
+	##Here only a post file is passed to template
