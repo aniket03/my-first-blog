d28be6b45357b5ac7f3f2024c6ff509df4447e07
aniket03 <aniket.bhatnagar0310@gmail.com>
2017-01-08 12:09:47 +0530

    Added forms to create/edit posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
+from django.shortcuts import redirect
 
 ## This function takes a request and will return render (put together) a template
 ## blog/post_list.html
@@ -17,3 +19,40 @@ def post_detail(request,pk):
 	post = get_object_or_404(Post,pk=pk)
 	return render(request,'blog/post_detail.html',{'post':post} )
 	##Here only a post file is passed to template
+
+def post_new(request):
+	if request.method=="POST":
+		form=PostForm(request.POST)
+		##To construct PostForm from data typed by user 
+		if form.is_valid():
+		##To check all fields are filled in properiety
+			post=form.save(commit=False)
+			##commit=0 so that data not saved to db directly
+			post.author=request.user
+			post.published_date=timezone.now()
+			post.save()
+			return redirect("post_detail",pk=post.pk)
+			##"post_detail" is name of the view want to got to
+	else:
+		form=PostForm()
+	return render(request,'blog/post_edit.html',{'form':form})
+
+def post_edit(request,pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method=="POST":
+		form=PostForm(request.POST,instance=post)
+		if form.is_valid():
+			post=form.save(commit=False)
+			post.author=request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect("post_detail",pk=post.pk)
+	else:
+		form=PostForm(instance=post)
+	return render(request,'blog/post_edit.html',{'form':form})
+
+
+
+
+
+
