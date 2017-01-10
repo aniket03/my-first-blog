5f989c25f51cf32728ffa4267c59afb0a023c6eb
aniket03 <aniket.bhatnagar0310@gmail.com>
2017-01-10 19:23:02 +0530

    Added comment model and functionalities

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Post
+from .models import Comment
 
 admin.site.register(Post)
+admin.site.register(Comment)
 ## To make our model visible on the admin page we need to register it with admin
